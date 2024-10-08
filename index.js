20a08fc0f7555fe2ed16ad98aef00511ac3b58a0
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-10-08 20:15:42 +0300

    toggle menu

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const burger = document.querySelector(".burger-menu");
-const menu = document.querySelector(".header-menu");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
-  menu.classList.toggle("active");
 });
