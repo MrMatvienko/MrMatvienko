53e2b5592bdceefc94180f2efb419aa5773f8ec9
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-10-10 13:26:22 +0300

    mobile modal menu

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const burger = document.querySelector(".burger-menu");
+const mobMenu = document.querySelector(".mobile-bar");
+const menuLinks = document.querySelectorAll(".mobile-bar-item a");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
+  mobMenu.classList.toggle("is-hidden");
+});
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    mobMenu.classList.add("is-hidden");
+  });
 });
