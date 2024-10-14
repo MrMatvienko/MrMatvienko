dc10b3ed3aa7ebe38ec7d891cb31054db523429a
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-10-14 20:46:42 +0300

    download cv

---
+++ projects.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    id: "fitness",
+    title: "Fitness App",
+    description:
+      "Fitness App - this is a web application for those who wish to have access to a database of exercises, with a detailed analysis of the execution technique.",
+    image: "./image/fitness.jpg",
+    role: "developer",
+    link: "https://mykyta-ushakow.github.io/Gym-squad/index.html",
+    technology: "HTML, CSS, JS",
+  },
+  {
+    id: "vagetables",
+    title: "Vagetables Store",
+    description:
+      "An organic products and fresh vegetables store for those who want to live a healthy lifestyle.",
+    image: "./image/vagetables.jpg",
+    role: "developer",
+    link: "https://iame1off.github.io/project-wonderful-nine/",
+    technology: "HTML, CSS, JS",
+  },
+  {
+    id: "taskpro",
+    title: "Task Manager",
+    description:
+      "A task manager for team collaboration, as well as for individuals who enjoy tracking task progress.",
+    image: "./image/taskpro.jpg",
+    role: "developer",
+    link: "https://annanadvor.github.io/taskPro_project-group-6/",
+    technology: "HTML, CSS, REACT JS",
+  },
+  {
+    id: "web-studio",
+    title: "Web-Studio",
+    description:
+      "Web-Studio - pet project focused on improving static layout development as well as general HTML and CSS concepts. It features responsive design with three breakpoints: 320px, 768px, and 1158px. Adaptive images were used for monitors with different pixel densities.",
+    image: "./image/web-studio.jpg",
+    role: "developer",
+    link: "https://mrmatvienko.github.io/web-studio/",
+    technology: "HTML, CSS, JS",
+  },
+  {
+    id: "rental-camper",
+    title: "Rental Camper",
+    description:
+      "Rental Camper - pet project utilizing Redux. A mock API service was used as the backend. The project was developed in a web format.",
+    image: "./image/rental-camper.jpg",
+    role: "developer",
+    link: "https://mrmatvienko.github.io/rental-campers/",
+    technology: "HTML, CSS, REACT JS, REDUX",
+  },
+  {
+    id: "slots",
+    title: "Slots playing",
+    description:
+      " Slots playing - pet project implemented with vanilla JavaScript, aimed at developing the ability to use asynchronous functions like setTimeout().",
+    image: "./image/slots.jpg",
+    role: "developer",
+    link: "https://mrmatvienko.github.io/SimpleSlot/",
+    technology: "HTML, CSS, JS",
+  },
+];
