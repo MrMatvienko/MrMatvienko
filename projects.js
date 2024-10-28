d25e5d0f8cc193b3ed171fd5c7b7cd01ad860950
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-10-28 11:37:37 +0200

    parcel install

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Fitness App",
     description:
       "Fitness App - this is a web application for those who wish to have access to a database of exercises, with a detailed analysis of the execution technique.",
-    image: "./image/fitness.jpg",
+    image: new URL("./image/fitness.jpg", import.meta.url).href,
     role: "developer",
     link: "https://mykyta-ushakow.github.io/Gym-squad/index.html",
     technology: "HTML, CSS, JS",
@@ -14,7 +14,7 @@ export const projects = [
     title: "Vagetables Store",
     description:
       "An organic products and fresh vegetables store for those who want to live a healthy lifestyle.",
-    image: "./image/vagetables.jpg",
+    image: new URL("./image/vagetables.jpg", import.meta.url).href,
     role: "developer",
     link: "https://iame1off.github.io/project-wonderful-nine/",
     technology: "HTML, CSS, JS",
@@ -24,7 +24,7 @@ export const projects = [
     title: "Task Manager",
     description:
       "A task manager for team collaboration, as well as for individuals who enjoy tracking task progress.",
-    image: "./image/taskpro.jpg",
+    image: new URL("./image/taskpro.jpg", import.meta.url).href,
     role: "developer",
     link: "https://annanadvor.github.io/taskPro_project-group-6/",
     technology: "HTML, CSS, REACT JS",
@@ -34,7 +34,7 @@ export const projects = [
     title: "Web-Studio",
     description:
       "Web-Studio - pet project focused on improving static layout development as well as general HTML and CSS concepts. It features responsive design with three breakpoints: 320px, 768px, and 1158px. Adaptive images were used for monitors with different pixel densities.",
-    image: "./image/web-studio.jpg",
+    image: new URL("./image/web-studio.jpg", import.meta.url).href,
     role: "developer",
     link: "https://mrmatvienko.github.io/web-studio/",
     technology: "HTML, CSS, JS",
@@ -44,7 +44,7 @@ export const projects = [
     title: "Rental Camper",
     description:
       "Movies Info is a pet project developed using React. An API is used to retrieve an up-to-date list of movies, along with descriptions, cast information, and reviews.",
-    image: "./image/films.jpg",
+    image: new URL("./image/films.jpg", import.meta.url).href,
     role: "developer",
     link: "https://mrmatvienko.github.io/movies-info/",
     technology: "HTML, CSS, REACT JS, Styled Components ",
@@ -54,9 +54,10 @@ export const projects = [
     title: "Slots playing",
     description:
       " Slots playing - pet project implemented with vanilla JavaScript, aimed at developing the ability to use asynchronous functions like setTimeout().",
-    image: "./image/slots.jpg",
+    image: new URL("./image/slots.jpg", import.meta.url).href,
     role: "developer",
     link: "https://mrmatvienko.github.io/SimpleSlot/",
     technology: "HTML, CSS, JS",
   },
 ];
+image: new URL("./image/films.jpg", import.meta.url).href;
